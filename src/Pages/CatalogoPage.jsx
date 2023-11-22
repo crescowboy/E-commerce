e55d4ebba281fd8b87c25f6779ba8a7eef034de0
wcrespo <wellingtoncrespo2001@gmail.com>
2023-11-21 23:47:05 -0500

    Agredando funcion aggCarrito a catalogo page

--- src/Pages/CatalogoPage.jsx
+++ src/Pages/CatalogoPage.jsx
@@ -1,10 +1,12 @@
 // src/components/CatalogoPage.js
-import React, { useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import ProductCard from './ProductCard';
 import Nav from '../Nav';
+import { Contexto } from '../Context/Contexto';
 
 const CatalogoPage = () => {
   const [products, setProducts] = useState([]);
+  const {cartItems, setCartItems} = useContext(Contexto)
 
   useEffect(() => {
     // Simula una llamada al backend para obtener la lista de productos
@@ -33,35 +35,35 @@ const CatalogoPage = () => {
           imageUrl: 'https://via.placeholder.com/200x200?text=Product3',
         },
         {
-          id: 1,
+          id: 4,
           name: 'Producto 1',
           description: 'Descripción del Producto 1.',
           price: 19.99,
           imageUrl: 'https://via.placeholder.com/200x200?text=Product1',
         },
         {
-          id: 2,
+          id: 5,
           name: 'Producto 2',
           description: 'Descripción del Producto 2.',
           price: 29.99,
           imageUrl: 'https://via.placeholder.com/200x200?text=Product2',
         },
         {
-          id: 3,
+          id: 6,
           name: 'Producto 3',
           description: 'Descripción del Producto 3.',
           price: 34.99,
           imageUrl: 'https://via.placeholder.com/200x200?text=Product3',
         },
         {
-          id: 2,
+          id: 7,
           name: 'Producto 2',
           description: 'Descripción del Producto 2.',
           price: 29.99,
           imageUrl: 'https://via.placeholder.com/200x200?text=Product2',
         },
         {
-          id: 3,
+          id: 8,
           name: 'Producto 3',
           description: 'Descripción del Producto 3.',
           price: 34.99,
@@ -76,6 +78,15 @@ const CatalogoPage = () => {
     fetchProducts();
   }, []);
 
+  const aggCarrito=(product)=>{
+    let cartItemsUpdate = [...cartItems];
+
+    cartItemsUpdate.push(product)
+    setCartItems(cartItemsUpdate)
+
+    console.log(cartItems)
+  }
+
   return (
     <div className="catalogo-container">
       <header>
@@ -84,7 +95,7 @@ const CatalogoPage = () => {
       <h1>Catálogo de Productos</h1>
       <div className="product-list">
         {products.map((product) => (
-          <ProductCard key={product.id} product={product} />
+          <ProductCard key={product.id} product={product} aggCarrito={aggCarrito} />
         ))}
       </div>
     </div>
